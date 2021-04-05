cae05d3452eecf9edc3c66038c4ebcd672422ef6
Daniel Rosenberg <drosen@google.com>
2021-04-05 16:27:51 +0530

    sm6150-common: Enable Project ID Quota support on userdata
    * These features enable wide inodes on userdata, which is required for project ID quotas. Wide inodes can only be set at MKFS time.
    * This also enables casefolding, which is required for the removal of SDCardFS.
    
    Bug: 138321217
    Test: Examine filesystem features for project quota and wide inodes.
    Change-Id: I9c17cda2af34c6fbd73c2d90eb6b465f4939df67
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -10,6 +10,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/sm6150-common/sm6150-common-vendor.mk)
 
+# Project ID Quota
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # A/B
 ifeq ($(TARGET_IS_VAB),true)
 # Inherit virtual_ab_ota product
